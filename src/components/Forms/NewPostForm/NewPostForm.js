d45b85213696c623942c297525929ea774c226e5
patgrajkowski <patgrajkowski@gmail.com>
2022-02-10 20:08:11 +0100

    added new categories

--- src/components/Forms/NewPostForm/NewPostForm.js
+++ src/components/Forms/NewPostForm/NewPostForm.js
@@ -71,43 +71,177 @@ const NewPostForm = ({ theme }) => {
       <span>
         <h4>Kategorie</h4>
         <div className={styles.form__categories}>
-          <input
-            type='checkbox'
-            value='languages'
-            id='languages'
-            name='languages'
-          />
-          <label htmlFor='languages'>Języki obce</label>
-          <input type='checkbox' value='math' name='math' id='math' />
-          <label htmlFor='math'>Matematyka</label>
-          <input type='checkbox' value='economy' id='economy' name='economy' />
-          <label htmlFor='economy'>Ekonomia</label>
-          <input
-            type='checkbox'
-            value='architecture'
-            id='architecture'
-            name='architecture'
-          />
-          <label htmlFor='architecture'>Architektura</label>
-          <input type='checkbox' value='it' id='it' name='it' />
-          <label htmlFor='it'>Informatyka</label>
-          <input type='checkbox' value='coding' name='coding' id='coding' />
-          <label htmlFor='coding'>Programowanie</label>
-          <input type='checkbox' value='biology' id='biology' name='biology' />
-          <label htmlFor='biology'>Biologia</label>
-          <input type='checkbox' value='history' id='history' name='history' />
-          <label htmlFor='history'>Historia</label>
-          <input type='checkbox' value='law' id='law' name='law' />
-          <label htmlFor='law'>Prawo</label>
-          <input type='checkbox' value='ethics' id='ethics' name='ethics' />
-          <label htmlFor='ethics'>Etyka</label>
-          <input
-            type='checkbox'
-            value='chemistry'
-            id='chemistry'
-            name='chemistry'
-          />
-          <label htmlFor='chemistry'>Chemia</label>
+          <div className={styles.categories__subcategory}>
+            <p className={styles.subcategory__name}>Języki obce</p>
+            <input
+              type='checkbox'
+              value='english'
+              name='english'
+              id='english'
+            />
+            <label htmlFor='english'>Angielski</label>
+            <input type='checkbox' value='dutch' name='dutch' id='dutch' />
+            <label htmlFor='dutch'>Niemiecki</label>
+            <input type='checkbox' value='french' name='french' id='french' />
+            <label htmlFor='french'>Francuski</label>
+            <input
+              type='checkbox'
+              value='spanish'
+              name='spanish'
+              id='spanish'
+            />
+            <label htmlFor='spanish'>Hiszpański</label>
+            <input
+              type='checkbox'
+              value='hungarian'
+              name='hungarian'
+              id='hungarian'
+            />
+            <label htmlFor='hungarian'>Węgierski</label>
+            <input
+              type='checkbox'
+              value='russian'
+              name='russian'
+              id='russian'
+            />
+            <label htmlFor='russian'>Rosyjski</label>
+          </div>
+          <div className={styles.categories__subcategory}>
+            <p className={styles.subcategory__name}>Humanistyczne</p>
+            <input type='checkbox' value='polish' id='polish' name='polish' />
+            <label htmlFor='polish'>Język polski</label>
+            <input
+              type='checkbox'
+              value='history'
+              id='history'
+              name='history'
+            />
+            <label htmlFor='history'>Historia</label>
+            <input
+              type='checkbox'
+              value='philosophy'
+              id='philosophy'
+              name='philosophy'
+            />
+            <label htmlFor='philosophy'>Filozofia</label>
+            <input type='checkbox' value='law' id='law' name='law' />
+            <label htmlFor='law'>Prawo</label>
+            <input type='checkbox' value='ethics' id='ethics' name='ethics' />
+            <label htmlFor='ethics'>Etyka</label>
+          </div>
+          <div className={styles.categories__subcategory}>
+            <p className={styles.subcategory__name}>Ścisłe</p>
+            <input type='checkbox' value='math' id='math' name='math' />
+            <label htmlFor='math'>Matematyka</label>
+            <input
+              type='checkbox'
+              value='robotics'
+              id='robotics'
+              name='robotics'
+            />
+            <label htmlFor='robotics'>Robotyka</label>
+            <input
+              type='checkbox'
+              value='electrotechnics'
+              id='electrotechnics'
+              name='electrotechnics'
+            />
+            <label htmlFor='electrotechnics'>Elektrotechnika</label>
+            <input type='checkbox' value='coding' id='coding' name='coding' />
+            <label htmlFor='coding'>Programowanie</label>
+            <input type='checkbox' value='it' id='it' name='it' />
+            <label htmlFor='it'>Informatyka</label>
+            <input
+              type='checkbox'
+              value='physics'
+              id='physics'
+              name='physics'
+            />
+            <label htmlFor='physics'>Fizyka</label>
+            <input
+              type='checkbox'
+              value='architecture'
+              id='architecture'
+              name='architecture'
+            />
+            <label htmlFor='architecture'>Architektura</label>
+          </div>
+          <div className={styles.categories__subcategory}>
+            <p className={styles.subcategory__name}>Przyrodnicze</p>
+            <input
+              type='checkbox'
+              value='biology'
+              id='biology'
+              name='biology'
+            />
+            <label htmlFor='biology'>Biologia</label>
+            <input
+              type='checkbox'
+              value='geography'
+              id='geography'
+              name='geography'
+            />
+            <label htmlFor='geography'>Geografia</label>
+            <input
+              type='checkbox'
+              value='chemistry'
+              id='chemistry'
+              name='chemistry'
+            />
+            <label htmlFor='chemistry'>Chemia</label>
+            <input
+              type='checkbox'
+              value='geodesy'
+              id='geodesy'
+              name='geodesy'
+            />
+            <label htmlFor='geodesy'>Geodezja</label>
+            <input
+              type='checkbox'
+              value='medicine'
+              id='medicine'
+              name='medicine'
+            />
+            <label htmlFor='medicine'>Medycyna</label>
+          </div>
+          <div className={styles.categories__subcategory}>
+            <p className={styles.subcategory__name}>Ekonomiczne</p>
+            <input
+              type='checkbox'
+              value='marketing'
+              id='marketing'
+              name='marketing'
+            />
+            <label htmlFor='marketing'>Marketing</label>
+            <input
+              type='checkbox'
+              value='counting'
+              id='counting'
+              name='counting'
+            />
+            <label htmlFor='counting'>Rachunkowość</label>
+            <input
+              type='checkbox'
+              value='finances'
+              id='finances'
+              name='finances'
+            />
+            <label htmlFor='finances'>Finanse</label>
+            <input
+              type='checkbox'
+              value='banking'
+              id='banking'
+              name='banking'
+            />
+            <label htmlFor='banking'>Bankowość</label>
+            <input
+              type='checkbox'
+              value='management'
+              id='management'
+              name='management'
+            />
+            <label htmlFor='management'>Zarządzanie</label>
+          </div>
         </div>
       </span>
       <span>
