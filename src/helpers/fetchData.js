a521c9c786f0ba7dedc6257e41f0aabc1367249b
Patryk Grajkowski <patrykgrajkowski@mbp-patryk.home>
2022-05-20 23:12:38 +0200

    Angular

--- src/helpers/fetchData.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const uri = 'https://helply-backend.herokuapp.com';
-export const fetchAccessToken = async (url) => {
-  const response = await axios.post(`${uri}/api/auth/refresh_token`);
-  const { accessToken } = response.data;
-  return accessToken;
-};
--- src/helpers/getNickname.js
+++
@@ -1,4 +0,0 @@
-export const getNickname = (nick, mail) => {
-  if (!nick) return mail.split('@')[0];
-  return nick;
-};
